4262f968e83ef940c3a151e69c86c8ea925e05af
Maciej Bratek <maclav3@gmail.com>
2017-12-19 22:46:50 +0100

    Add setup

---
+++ setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+from distutils.core import setup
+
+setup(
+    name='tplink-util',
+    version='1.0',
+    description='A cli util to control TP-Link smart bulbs with cool features',
+    author='Maciej Bratek',
+    author_email='maclav3@gmail.com',
+    url='https://github.com/maclav3/tplink-util',
+    packages=['tplink-util'],
+    requires=['pyHS100']
+)
