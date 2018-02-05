bb41ccb20bfadbb4e968a341457a18de2647a662
Maciej Bratek <maclav3@gmail.com>
2018-02-05 20:11:56 +0100

    Add requirements and prepare for the use of virtual env

--- setup.py
+++ setup.py
@@ -5,17 +5,12 @@ from distutils.core import setup
 setup(
     name='bulbutil',
     version='1.0',
-    description='A cli util to control smart bulbs with cool features. The TP-Link smart bubl interface is implemented'
+    description='A cli util to control smart bulbs with cool features. The TP-Link smart bulb interface is implemented'
                 'for the time being. It may be easily extended with other bulb interfaces.',
     author='Maciej Bratek',
     author_email='maclav3@gmail.com',
     url='https://github.com/maclav3/bulbutil',
     packages=['bulbutil'],
-    requires=[
-        'pyHS100',
-        'colorlog',
-        'GeoIP', 'tzlocal', 'pytz', 'pysolar', 'numpy', 'arrow', 'configargparse'
-    ],
     entry_points={
         'console_scripts': [
             'bulbutil = modes.__main__:main'
